d6daabb985762750f3de9474a9afee2f9a039d43
Jakub Motyl <kubamotyl16@gmail.com>
2026-03-19 19:28:50 +0100

    Finalize Calendar view design on desktop

--- src/components/GridCell.tsx
+++ src/components/GridCell.tsx
@@ -1,15 +1,32 @@
 import type { CalendarEvent } from "../types";
 
 interface GridCellProps {
-  day: number | null;
-  dailyEvents: CalendarEvent[];
+    day: number | null;
+    dailyEvents: CalendarEvent[];
 }
 
 export default function GridCell({ day, dailyEvents }: GridCellProps) {
-  console.log(dailyEvents);
-  return (
-    <div className="min-h-24 box-padding flex flex-col">
-      <span>{day ? day : "-"}</span>
-    </div>
-  );
+    console.log(dailyEvents);
+    return (
+        <div className="h-24 px-2 py-1 flex flex-col gap-0.5 overflow-y-auto bg-white">
+            <span className="font-black text-accent text-xs">{day || "-"}</span>
+            <div className="flex flex-col gap-1">
+                {dailyEvents.map((event, id) => (
+                    <div
+                        key={id}
+                        // Events with status "played" are marked with bright red, events with status "scheduled" are marked with light green
+                        className={`flex items-center gap-1 text-xs p-px rounded cursor-pointer ${event.status === "played" ? "bg-red-300" : "bg-green-300"}`}
+                    >
+                        <p className="font-medium">
+                            {event.timeVenueUTC?.slice(0, 5) || "TBA"}
+                        </p>
+                        <span className="truncate">
+                            {event.homeTeam?.officialName || "TBA"} vs{" "}
+                            {event.awayTeam?.officialName || "TBA"}
+                        </span>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,26 +1,26 @@
-interface Team {
-  officialName: string;
+export interface Team {
+    officialName: string;
 }
 
-interface MatchResult {
-  homeGoals: number;
-  awayGoals: number;
-  winner: string | null;
+export interface MatchResult {
+    homeGoals: number;
+    awayGoals: number;
+    winner: string | null;
 }
 
-interface MatchStage {
-  id: string;
-  name: string;
+export interface MatchStage {
+    id: string;
+    name: string;
 }
 
 export interface CalendarEvent {
-  status: string;
-  dateVenue: string;
-  timeVenueUTC: string;
-  stadium: string | null;
-  homeTeam: Team | null;
-  awayTeam: Team | null;
-  result: MatchResult | null;
-  stage: MatchStage;
-  originCompetitionName: string;
+    status: string;
+    dateVenue: string;
+    timeVenueUTC: string;
+    stadium: string | null;
+    homeTeam: Team | null;
+    awayTeam: Team | null;
+    result: MatchResult | null;
+    stage: MatchStage;
+    originCompetitionName: string;
 }
